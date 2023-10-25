9a26de7ab88ed38f15839b94ffbe6398dd17efe3
Simon Skinner <vultuk28@gmail.com>
2023-10-25 11:34:42 +0100

    WIP

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,7 @@ import {
 } from '@react-navigation/native';
 import { SplashScreen, Stack } from 'expo-router';
 
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useColorScheme } from 'react-native';
-import { useEffect } from 'react';
-import { useFonts } from 'expo-font';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -24,26 +21,6 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return <RootLayoutNav />;
 }
 
